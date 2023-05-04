282d16d8dc38ab23cad8292ed87553824567c319
Mike <michaelxu17@gmail.com>
2023-05-05 00:16:55 +0700

    add first char

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVZmWnM5di8vLy8vQUFBQm1UcCt6bkc4V1VHUW84Y1lwcHJiQlAxeWJKSFZiUFNKZHJMNFdRT2doVW5keW91R1l0QTJhVWR2T21XbVlOZmc1QjAxekZzMXpHWU90dzBtNkV1RkpXR0xEVDM2UlNaS0kxaVNaWFFDWGY4MlRQVmxhNW8yOTFzNnlHeWFpRC9XUnVWdEc0M3R1N2ZHMDBmV0pNYkViZzk1QTNVUlQ3ZG1tZlZpS0ZzRmtOY3NBNWprazNYeXNWUEtFeEtIUUZPWjVNSzNQQW5VM1FTZ1dzcythN0FjQlJlcmhvY0FGaThja0M5UGYyM0tTTm5GMDBBMlRyWDJQOTczcFo3RWNjMzVHMEpXRlVTVE4zaVBSb08vR0NEdU9qRXA2a3hmeHhzeHFma3A2UVVrTG03VmRzZ2hqRzBCdjdxSnE0cURtcXNBNkdiU1kyRGZnMXR1SFltem9hSlNUMkJsalJKVmlkMlJGODZFOGVNNDBZaGk=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
